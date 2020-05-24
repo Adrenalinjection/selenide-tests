11d20b100f91ad990a0b05db11dc9a142703b2f9
Maria Klichko <65122060+Adrenalinjection@users.noreply.github.com>
2020-05-24 18:24:02 +0300

    my first commit

---
+++ src/test/java/tests/GoogleTests.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+class GoogleTests {
+
+    @Test
+    void successfulSearch() {
+        open("http://google.com");
+
+        $(byName("q")).val("lepra").pressEnter();
+
+        $("html").shouldHave(text("Лепрозорий: вход"));
+    }
+
+
+    @Test
+    void successfulSearch1() {
+        open("http://google.com");
+
+        $(byName("q")).val("lepra").pressEnter();
+
+        $("html").shouldHave(text("Лепрозорий: вход"));
+    }
+}
