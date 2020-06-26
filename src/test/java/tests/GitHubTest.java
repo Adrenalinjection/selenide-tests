5be50c0015bf624e6af575ce5e35d33bc99d4d8e
Maria Klichko <65122060+Adrenalinjection@users.noreply.github.com>
2020-06-26 23:51:37 +0300

    added yandex_tests

---
+++ src/test/java/tests/GitHubTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import org.junit.Test;
+
+public class GitHubTest {
+
+    @Test
+    public void testIssueExists() {
+            }
+}
--- src/test/java/tests/YandexTests.java
+++ src/test/java/tests/YandexTests.java
@@ -2,19 +2,20 @@ package tests;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 @Tag("search")
-class GoogleTests {
+class YandexTests {
 
     @Test
     void successfulSearch() {
-        open("http://google.com");
+        open("http://ya.ru");
 
-        $(byName("q")).val("lepra").pressEnter();
+        $(byName("text")).val("lepra").pressEnter();
 
         $("html").shouldHave(text("Лепрозорий: вход"));
     }
