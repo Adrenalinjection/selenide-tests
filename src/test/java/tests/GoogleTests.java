8128bf3bcdab0a0b5fdd9d2e828953e9c31bd9fc
Maria Klichko <65122060+Adrenalinjection@users.noreply.github.com>
2020-06-30 22:35:29 +0300

    added selenoid

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -1,17 +1,27 @@
 package tests;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
+@Epic("QA.GURU automation course")
+@Story("Google test")
 @Tag("search")
 class GoogleTests extends TestBase {
 
     @Test
-    void successfulSearch() {
+    @Description("Google test, we look for lepra")
+    @DisplayName("Google test")
+        void successfulSearch() {
         open("http://google.com");
 
         $(byName("q")).val("lepra").pressEnter();
@@ -19,5 +29,15 @@ class GoogleTests extends TestBase {
         $("html").shouldHave(text("Лепрозорий: вход"));
     }
 
+        @Test
+        @Description("Google test, we look for 2ch but cannot find")
+        @DisplayName("Google test")
+        void unsuccessfulSearch() {
+            open("http://google.com");
+
+            $(byName("q")).val("2ch").pressEnter();
+
+            $("html").shouldHave(text("Лепрозорий: вход"));
+        }
 
 }
